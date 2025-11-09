9471378333585c37670c16caadf07bef0ef85e27
Christian Proinger <christian.proinger@gmail.com>
2025-11-09 20:39:52 +0100

    auf deutsch übersetzen

---
+++ gegner.lua
@@ -0,0 +1,31 @@
+
+
+local Gegner = { geschwindigkeit = 70, ziel = nil }
+Gegner.__index = Gegner
+
+function Gegner.new()
+    local self = setmetatable({}, Gegner)
+    return self
+end
+
+function Gegner:update(dt, spieler)
+    if (spieler.letztePositionen[10]) then
+        self.ziel = {}
+        self.ziel.x = spieler.letztePositionen[10].x
+        self.ziel.y = spieler.letztePositionen[10].y
+    end 
+    
+    if (self.ziel) then
+        local dirX = self.ziel.x - self.x
+        local dirY = self.ziel.y - self.y
+        local length = math.sqrt(dirX * dirX + dirY * dirY)
+        if length > 10 then
+            dirX = dirX / length
+            dirY = dirY / length
+            self.x = self.x + dirX * self.geschwindigkeit * dt
+            self.y = self.y + dirY * self.geschwindigkeit * dt
+        end
+    end
+end
+
+return Gegner
--- main.lua
+++ main.lua
@@ -1,48 +1,43 @@
 
 
 function love.load()
-    player = {}
-    player.x = 100
-    player.y = 200
-    player.locationHistory = {}
-    player.speed = 100
+    spieler = {}
+    spieler.x = 100
+    spieler.y = 200
+    spieler.letztePositionen = {}
+    spieler.geschwindigkeit = 100
 
-    local Enemy = require("enemy")
-    enemy = Enemy.new()
-    enemy.x = 800
-    enemy.y = 200
+    local Gegner = require("gegner")
+    gegner = Gegner.new()
+    gegner.x = 800
+    gegner.y = 200
 end
 
 function love.update(dt)
     
     if (love.keyboard.isDown("right")) then
-        player.x = player.x + player.speed * dt
+        spieler.x = spieler.x + spieler.geschwindigkeit * dt
     end
     if (love.keyboard.isDown("left")) then
-        player.x = player.x - player.speed * dt
+        spieler.x = spieler.x - spieler.geschwindigkeit * dt
     end
     if (love.keyboard.isDown("down")) then
-        player.y = player.y + player.speed * dt
+        spieler.y = spieler.y + spieler.geschwindigkeit * dt
     end
     if (love.keyboard.isDown("up")) then
-        player.y = player.y - player.speed * dt
+        spieler.y = spieler.y - spieler.geschwindigkeit * dt
     end
     
-    table.insert(player.locationHistory, 1, {x = player.x, y = player.y})
-    table.remove(player.locationHistory, 11)  -- Keep only the last 10 positions
-    if (player.locationHistory[10]) then
-        enemy.target = {}
-        enemy.target.x = player.locationHistory[10].x
-        enemy.target.y = player.locationHistory[10].y
-    end 
+    table.insert(spieler.letztePositionen, 1, {x = spieler.x, y = spieler.y})
+    table.remove(spieler.letztePositionen, 11)  -- Keep only the last 10 positions
 
-    enemy:update(dt, player)
+    gegner:update(dt, spieler)
 end
 
 function love.draw()
     love.graphics.setColor(0, 1, 0)  -- Set color to green for the player
-    love.graphics.rectangle("fill", player.x, player.y, 50, 50)
+    love.graphics.rectangle("fill", spieler.x, spieler.y, 50, 50)
 
     love.graphics.setColor(1, 0, 0)  -- Set color to red for the enemy
-    love.graphics.rectangle("fill", enemy.x, enemy.y, 50, 50)
+    love.graphics.rectangle("fill", gegner.x, gegner.y, 50, 50)
 end
